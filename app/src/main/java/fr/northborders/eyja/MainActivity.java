0ba7c42f7811427882ff6f049fefa86adad055cb
Thibault Guegan <thibault.guegan@gmail.com>
2015-01-02 20:18:12 +0100

    Deleting unnecessary classes

--- app/src/main/java/fr/northborders/eyja/MainActivity.java
+++ app/src/main/java/fr/northborders/eyja/MainActivity.java
@@ -1,9 +1,5 @@
 package fr.northborders.eyja;
 
-import java.util.Locale;
-
-import android.content.Intent;
-import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -12,16 +8,12 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import fr.northborders.eyja.gui.SlidingTabLayout;
 
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener{
