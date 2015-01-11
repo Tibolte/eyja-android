7d80f8e077c76fc1f73a05650735b8680e355205
Thibault Guegan <thibault.guegan@gmail.com>
2015-01-11 15:14:11 +0000

    Getting rid of old code

--- app/src/main/java/fr/northborders/eyja/MainActivity.java
+++ app/src/main/java/fr/northborders/eyja/MainActivity.java
@@ -1,38 +1,24 @@
 package fr.northborders.eyja;
 
 import android.app.Activity;
-import android.content.Context;
-import android.support.v4.view.PagerAdapter;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import flow.Backstack;
 import flow.Flow;
-import fr.northborders.eyja.presenters.BasePresenter;
-import fr.northborders.eyja.presenters.DummyPresenter;
-import fr.northborders.eyja.presenters.RootPresenter;
-import fr.northborders.eyja.utils.FlowBundler;
-import fr.northborders.eyja.utils.GsonParcer;
-import fr.northborders.eyja.utils.Section;
 import hugo.weaving.DebugLog;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class MainActivity extends Activity implements Flow.Listener{
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private FrameLayout switcher;
+    @InjectView(R.id.switcher)
+    FrameLayout switcher;
 
     /**
      * MARK: Lifecycle methods
@@ -43,8 +29,7 @@ public class MainActivity extends Activity implements Flow.Listener{
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        switcher = (FrameLayout) findViewById(R.id.switcher);
+        ButterKnife.inject(this);
     }
 
     @Override
