1e02b0ac1df10fdeeede9133afc3f168560a4db8
Thibault Guegan <thibault.guegan@gmail.com>
2015-01-28 02:19:13 +0000

    Multiple tasks parallelized

--- app/src/main/java/fr/northborders/eyja/ui/TransitionAdapter.java
+++ app/src/main/java/fr/northborders/eyja/ui/TransitionAdapter.java
@@ -1,10 +1,13 @@
 package fr.northborders.eyja.ui;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.transition.Transition;
 
 /**
  * Created by thibaultguegan on 26/01/15.
  */
+@TargetApi(Build.VERSION_CODES.KITKAT)
 public class TransitionAdapter implements Transition.TransitionListener {
     @Override
     public void onTransitionStart(Transition transition) {
--- app/src/main/java/fr/northborders/eyja/util/Keys.java
+++ app/src/main/java/fr/northborders/eyja/util/Keys.java
@@ -7,4 +7,5 @@ public class Keys {
     public static final String KEY_TITLE       = "title";
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_PHOTO       = "photo";
+    public static final String KEY_CONTENT = "content";
 }
